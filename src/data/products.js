4c1cdfb760b143bc7834072284fe978ef27151d5
VitalPet <vitalpetld@gmail.com>
2025-11-12 20:13:53 +0100

    fix: spa rewrites + vite base

--- src/data/products.js
+++ src/data/products.js
@@ -21,6 +21,31 @@ export const PRODUCTS = [
   { id: "rc-016", name: "Gastrointestinal Low Fat Small Dog 3.5KG", brand: "Royal Canin", age: "Chiot", price: 62.49, pet: "chien", cat: "Croquettes", feature: "", size: "3.5kg", img: "https://i.postimg.cc/Sx9tQMTK/07cbeb43-e6b1-4a78-b164-b2490009ed97-Download-As-Jpg.webp" },
   { id: "rc-017", name: "Gastrointestinal Low Fat Small Dog 8KG", brand: "Royal Canin", age: "Chiot", price: 87.17, pet: "chien", cat: "Croquettes", feature: "", size: "8kg", img: "https://i.postimg.cc/Sx9tQMTK/07cbeb43-e6b1-4a78-b164-b2490009ed97-Download-As-Jpg.webp" },
   { id: "rc-018", name: "Maxi Dental Care 9KG", brand: "Royal Canin", age: "Chiot", price: 57.00, pet: "chien", cat: "Croquettes", feature: "", size: "9kg", img: "https://i.postimg.cc/6q7fHkFQ/maxidentalcare-600x.webp" },
+ 
+
+  { id: "rcC-001", name: "Urinary Care 2kg", brand: "Royal Canin", age: "Chaton", price: 32.49, pet: "chat", cat: "Croquettes", feature: "", size: "2kg",  img: "https://i.postimg.cc/153rYqh0/6206634-1-r800x800.jpg" },
+  { id: "rcC-002", name: "Urinary Care 4kg", brand: "Royal Canin", age: "Chaton", price: 55.39, pet: "chat", cat: "Croquettes", feature: "", size: "4kg",  img: "https://i.postimg.cc/153rYqh0/6206634-1-r800x800.jpg" },
+  { id: "rcC-003", name: "Urinary Care 10kg", brand: "Royal Canin", age: "Chaton", price: 110.39, pet: "chat", cat: "Croquettes", feature: "", size: "10kg", img: "https://i.postimg.cc/153rYqh0/6206634-1-r800x800.jpg" },
+
+  { id: "rcC-004", name: "Sterilised Ageing 11+ 2kg", brand: "Royal Canin", age: "Chaton", price: 30.39, pet: "chat", cat: "Croquettes", feature: "", size: "2kg",  img: "https://i.postimg.cc/44qxjDr3/3182551065947-12.webp" },
+  { id: "rcC-005", name: "Sterilised Ageing 11+ 4kg", brand: "Royal Canin", age: "Chaton", price: 51.79, pet: "chat", cat: "Croquettes", feature: "", size: "4kg",  img: "https://i.postimg.cc/44qxjDr3/3182551065947-12.webp" },
+
+  { id: "rcC-006", name: "Kitten Sterilised pour Chaton 2kg", brand: "Royal Canin", age: "Chaton", price: 31.29, pet: "chat", cat: "Croquettes", feature: "", size: "2kg",  img: "https://i.postimg.cc/1XwXvMRm/3182550805186-2.webp" },
+  { id: "rcC-007", name: "Kitten Sterilised pour Chaton 3.5kg", brand: "Royal Canin", age: "Chaton", price: 44.59, pet: "chat", cat: "Croquettes", feature: "", size: "3.5kg", img: "https://i.postimg.cc/1XwXvMRm/3182550805186-2.webp" },
+
+  { id: "rcC-008", name: "Dental Care 1.5kg", brand: "Royal Canin", age: "Chaton", price: 24.39, pet: "chat", cat: "Croquettes", feature: "", size: "1.5kg", img: "https://i.postimg.cc/qR8HYFng/b7b15577-3a86-4bac-9ea0-b2490007b505-Download-As-Jpg.webp" },
+  { id: "rcC-009", name: "Dental Care 3.5kg", brand: "Royal Canin", age: "Chaton", price: 46.29, pet: "chat", cat: "Croquettes", feature: "", size: "3.5kg", img: "https://i.postimg.cc/qR8HYFng/b7b15577-3a86-4bac-9ea0-b2490007b505-Download-As-Jpg.webp" },
+  { id: "rcC-010", name: "Dental Care 8kg", brand: "Royal Canin", age: "Chaton", price: 88.29, pet: "chat", cat: "Croquettes", feature: "", size: "8kg",  img: "https://i.postimg.cc/qR8HYFng/b7b15577-3a86-4bac-9ea0-b2490007b505-Download-As-Jpg.webp" },
+
+  { id: "rcC-011", name: "Light Weight Care 1.5kg", brand: "Royal Canin", age: "Chaton", price: 24.39, pet: "chat", cat: "Croquettes", feature: "", size: "1.5kg", img: "https://i.postimg.cc/cHc5fGwv/Royal-Canin-Light-Weight-Care-1-5kg.jpg" },
+  { id: "rcC-012", name: "Light Weight Care 3kg",   brand: "Royal Canin", age: "Chaton", price: 41.49, pet: "chat", cat: "Croquettes", feature: "", size: "3kg",  img: "https://i.postimg.cc/cHc5fGwv/Royal-Canin-Light-Weight-Care-1-5kg.jpg" },
+  { id: "rcC-013", name: "Light Weight Care 8kg",   brand: "Royal Canin", age: "Chaton", price: 88.29, pet: "chat", cat: "Croquettes", feature: "", size: "8kg",  img: "https://i.postimg.cc/cHc5fGwv/Royal-Canin-Light-Weight-Care-1-5kg.jpg" },
+
+  { id: "rcC-014", name: "Mother and Babycat pour chatte et chaton 2kg", brand: "Royal Canin", age: "Chaton", price: 32.69, pet: "chat", cat: "Croquettes", feature: "", size: "2kg", img: "https://i.postimg.cc/GmYKk1nZ/659193-a395309c652349e88288b7d0463cee2f-mv2.avif" },
+  { id: "rcC-015", name: "Mother and Babycat pour chatte et chaton 4kg", brand: "Royal Canin", age: "Chaton", price: 55.69, pet: "chat", cat: "Croquettes", feature: "", size: "4kg", img: "https://i.postimg.cc/GmYKk1nZ/659193-a395309c652349e88288b7d0463cee2f-mv2.avif" },
+
+  { id: "rcC-016", name: "Sterilised 37 4kg",  brand: "Royal Canin", age: "Chaton", price: 44.99, pet: "chat", cat: "Croquettes", feature: "", size: "4kg",  img: "https://i.postimg.cc/zB3RFnR7/659193-311755a9d71741b8ac57ad6c713c9872-mv2.avif" },
+  { id: "rcC-017", name: "Sterilised 37 10kg", brand: "Royal Canin", age: "Chaton", price: 89.69, pet: "chat", cat: "Croquettes", feature: "", size: "10kg", img: "https://i.postimg.cc/zB3RFnR7/659193-311755a9d71741b8ac57ad6c713c9872-mv2.avif" },
 ];
 
 export default PRODUCTS;
